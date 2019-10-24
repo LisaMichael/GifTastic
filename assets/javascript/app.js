dc0d38f08d72ec3bd86e5553de509fa556d6af6c
Lisa <lisa.michael@hotmail.com>
2019-10-23 22:36:32 -0400

    got the animal giphy appearing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,90 @@
 $(document).ready(function () {
 
-// Initial array of giphy items to search on 
-let giphyTopic = [];
+  // Initial array of giphy items to search on 
+  let animalArray = ["dog"];
 
-// Function for displaying movie data
-function renderButtons() {
+  // Function for displaying movie data
+  function renderButtons() {
 
-  // Deleting the previous buttons prior to adding new movie buttons
-  // (this is necessary otherwise we will have repeat buttons)
-  $("#buttons-view").empty();
+    // Deleting the previous buttons prior to adding new movie buttons
+    // (this is necessary otherwise we will have repeat buttons)
+    $("#buttons-view").empty();
 
-  // Looping through the array of movies
-  for (let i = 0; i < giphyTopic.length; i++) {
+    // Looping through the array of movies
+    for (let i = 0; i < animalArray.length; i++) {
 
-    // Then dynamicaly generating buttons for each movie in the array.
-    // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-    var a = $("<button>");
-    // Adding a class
-    a.addClass("giph");
-    // Adding a data-attribute with a value of the movie at index i
-    a.attr("data-topic", giphyTopic[i]);
-    // Providing the button's text with a value of the movie at index i
-    a.text(giphyTopic[i]);
-    // Adding the button to the HTML
-    $("#buttons-view").append(a);
+      // Then dynamicaly generating buttons for each movie in the array.
+      // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+      let a = $("<button>");
+      // Adding animal and btn class
+      a.addClass("animal");
+      // Adding a data-attribute with a value of the movie at index i
+      a.attr("data-critter", animalArray[i]);
+      // Providing the button's text with a value of the movie at index i
+      a.text(animalArray[i]);
+      // Adding the button to the HTML
+      $("#buttons-view").append(a);
+    }
   }
-}
 
 
-// This function handles events where one button is clicked
-$("#add-topic").on("click", function (event) {
-  // event.preventDefault() prevents the form from trying to submit itself.
-  // We're using a form so that the user can hit enter instead of clicking the button if they want
-  event.preventDefault();
+  // This function handles events where one button is clicked
+  $("#add-animal").on("click", function (event) {
+    // event.preventDefault() prevents the form from trying to submit itself.
+    // We're using a form so that the user can hit enter instead of clicking the button if they want
+    event.preventDefault();
 
-  // This line will grab the text from the input box
-  let giphy = $("#topic-input").val().trim();
-  // giphy inut from the textbox is then added to giphyTopic array
-  giphyTopic.push(giphy);
+    // This line will grab the text from the input box
+    let animal = $("#animal-input").val().trim();
+    // animal input from the textbox is then added to animalArray array
+    animalArray.push(animal);
 
-  // calling renderButtons which handles the processing of our movie array renderButtons();
+    // calling renderButtons which handles the processing of our animal array renderButtons();
+    renderButtons();
+
+    
+  });
+
+
+ 
+
+
+  // Calling the renderButtons function at least once to display the initial list of movies
   renderButtons();
-});
 
-// Calling the renderButtons function at least once to display the initial list of movies
-renderButtons();
+  animalClick(); 
+
+  function animalClick(){ 
+  $(".animal").on("click", function () {
+    let animal = $(this).attr("data-critter");
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=aTWv3RucDbeLALuro6mLkPuHuTvdhzov&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function (response) {
+        let results = response.data;
+
+        for (let i = 0; i < results.length; i++) {
+          let gifDiv = $("<div>");
+
+          let rating = results[i].rating;
+
+          let p = $("<p>").text("Rating: " + rating);
+
+          let animalImage = $("<img>");
+          animalImage.attr("src", results[i].images.fixed_height.url);
+
+          gifDiv.prepend(p);
+          gifDiv.prepend(animalImage);
+
+          $("#gifs-appear-here").prepend(gifDiv);
+        }
+      });
+  });
+
+}  
+// end of animalClick function ()
 
 });
